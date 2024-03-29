8fd2bf1c3eaf560a05c266568e40fc1d9e7107ea
Himanshu <himvarandani@gmail.com>
2024-03-30 00:22:29 +0530

    Add tailwind css and create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header className="flex justify-end bg-gray-100 py-2 px-20">
+        <button
+          className="bg-white border-2 border-blue-800 rounded-xl text-blue-800 font-extrabold py-2 px-8"
         >
-          Learn React
-        </a>
+          Save Changes
+        </button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
