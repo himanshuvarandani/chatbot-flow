ab5dbd766ab984a6524aa1ecf507065bd558f352
Himanshu <himvarandani@gmail.com>
2024-03-31 00:56:57 +0530

    Create header and add save functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,17 @@ import NodesPanel from "./comonents/NodesPanel"
 import SettingsPanel from "./comonents/SettingsPanel"
 import { NodeType } from "../types"
 import "reactflow/dist/style.css"
+import Header from "./comonents/Header"
 
 function App() {
   const [selectedNode, setSelectedNode] = useState<NodeType | null>(null)
 
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="flex justify-end bg-gray-100 py-2 px-20">
-        <button
-          className="bg-white border-2 border-blue-800 rounded-xl text-blue-800 font-extrabold py-2 px-8"
-        >
-          Save Changes
-        </button>
-      </header>
+      <ReactFlowProvider>
+        <Header />
 
-      <div className="flex flex-auto">
-        <ReactFlowProvider>
+        <div className="flex flex-auto">
           <ChatbotFlow
             selectedNode={selectedNode}
             setSelectedNode={setSelectedNode}
@@ -36,8 +31,8 @@ function App() {
               )
             }
           </div>
-        </ReactFlowProvider>
-      </div>
+        </div>
+      </ReactFlowProvider>
     </div>
   )
 }
---
+++ src/comonents/Header.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react"
+import { useReactFlow } from "reactflow"
+
+const Header = () => {
+  const reactFlow = useReactFlow()
+  const [showModal, setShowModal] = useState(false)
+
+  const save = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    const nodes = reactFlow.getNodes()
+    const edges = reactFlow.getEdges()
+
+    // Count unconnected source handles
+    let unConnectedSources = 0
+    // Check for each node and edge combination to count unconncected source handles
+    nodes.forEach(node => {
+      if (!edges.filter(edge => edge.source === node.id).length)
+        unConnectedSources += 1
+    })
+
+    if (unConnectedSources > 1) {
+      setShowModal(true)
+      setTimeout(() => setShowModal(false), 3000)
+    } else {
+      // After this call flow save api
+      alert("Changes Saved")
+    }
+  }
+
+  return (
+    <>
+      <header className="flex justify-end bg-gray-100 py-2 px-20">
+        <button
+          className="bg-white border-2 border-blue-800 rounded-xl text-blue-800 font-extrabold py-2 px-8"
+          onClick={save}
+        >
+          Save Changes
+        </button>
+      </header>
+      
+      <div
+        className={`fixed top-3 left-1/2 -translate-x-1/2 bg-red-200 rounded-xl
+          py-2 px-4 transition-opacity ease-in-out duration-1000
+          ${showModal ? "opacity-100" : "opacity-0 hidden"}
+        `}
+      >
+        <p className="font-bold">Cannot save flow</p>
+      </div>
+    </>
+  )
+}
+
+export default Header
